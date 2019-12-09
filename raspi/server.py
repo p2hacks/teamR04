d9afc85d437ad83f86e6655f690696daa8c296fc
materialofmouse <materialofmouse@gmail.com>
2019-12-09 22:12:33 +0900

    make json data from raspberrypi's infomations

--- raspi/server.py
+++ raspi/server.py
@@ -4,7 +4,7 @@ import requests
 from flask import Flask
 
 app = Flask(__name__)
-server_url = 'http://133.242.224.119:5000/'
+SERVER_URL = 'http://133.242.224.119:5000/'
 
 @app.route("/")
 def root():
@@ -13,8 +13,8 @@ def root():
 def get_mac_address():
 	return str(uuid.UUID(int=uuid.getnode()).hex[-12:])
 
-if __name__ == '__main__':
-	res = requests.post(server_url, data)
+if __name__ == '__main__':	
+	data = json.dump({'mac_address': get_mac_address()})
+	res = requests.post(SERVER_URL, data)
 	print(res)
-	data = json.dump({'mac_address': get_mac_address(), ''})
 	app.run(host='0.0.0.0')
