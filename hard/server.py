2f2e9faf2a5e6620418437147528b29af7266c70
materialofmouse <materialofmouse@gmail.com>
2019-12-11 01:12:08 +0900

    add header

--- hard/server.py
+++ hard/server.py
@@ -2,6 +2,7 @@ import json
 import uuid
 import requests
 import socket
+from time import sleep
 from flask import Flask
 
 app = Flask(__name__)
@@ -11,6 +12,10 @@ SERVER_URL = 'http://133.242.224.119:5000/'
 def root():
 	return "ok\n"
 
+@app.route("/down", methods=['GET'])
+def down():
+	print('down!!! servo!!!')
+
 def get_mac_address():
 	return str(uuid.UUID(int=uuid.getnode()).hex[-12:])
 
@@ -18,6 +23,6 @@ def get_ip_address():
 	return str(socket.gethostbyname(socket.gethostname()))
 
 if __name__ == '__main__':
-	data = json.dumps({'mac_address' : get_mac_address(), 'ip_address' : get_ip_address(), 'latitude' : "11111", 'longitude' : "22222"})
-	res = requests.post(SERVER_URL, json.loads(data))
+	data = json.dumps({'mac_address' : get_mac_address(), 'ip_address' : get_ip_address(), 'latitude' : 11111, 'longitude' : 22222})
+	res = requests.post(SERVER_URL, data=json.dumps(data), headers={'content-type': 'application/json'})
 	app.run(host='0.0.0.0')
