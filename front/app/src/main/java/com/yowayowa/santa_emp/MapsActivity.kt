5002437845a43eccd4247a6b10771a340e2805fe
yuhi-dosei <yuhi.dosei@gmail.com>
2019-12-14 12:25:31 +0900

    add drawer

--- front/app/src/main/java/com/yowayowa/santa_emp/MapsActivity.kt
+++ front/app/src/main/java/com/yowayowa/santa_emp/MapsActivity.kt
@@ -28,7 +28,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_maps)
+        setContentView(R.layout.fragment_home)
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
